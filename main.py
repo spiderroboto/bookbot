35ebc1dcfa7710c41dbcdae29041688373b08d5c
siderroboto <robottoeclipse52@gmail.com>
2025-12-13 07:31:05 -0700

    fully working bookbot open source for everyone

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+import sys
 from stats import words_count, character_count
 
-with open("books/frankenstein.txt", "r") as f:
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+book_path = sys.argv[1]
+
+with open(book_path, "r") as f:
     content = f.read()
 
-total = words_count(content)
-print(f"Found {total} total words")
-
+total_words = words_count(content)
 char_counts = character_count(content)
-print(char_counts)
+
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {book_path}...")
+print("----------- Word Count ----------")
+print(f"Found {total_words} total words")
+print("--------- Character Count -------")
+
+sorted_chars = sorted(char_counts.items(), key=lambda x: x[1], reverse=True)
+
+for char, count in sorted_chars:
+    if char.isalpha():
+        print(f"{char}: {count}")
+
+print("============= END ===============")
